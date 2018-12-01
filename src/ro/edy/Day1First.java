b156cdd6171c664c81f4efaa21cb47035f10ea59
EdyTheDeveloper <sparky_2oo4_2o@yahoo.com>
2018-12-01 21:04:50 +0200

    First challenge

--- src/ro/edy/Day1First.java
+++ src/ro/edy/Day1First.java
@@ -1,10 +1,52 @@
 package ro.edy;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Day1First {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String input = null;
 
+	public Day1First() {
+		initializeInput();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Day1First().processInput());
+	}
+
+	protected String processInput() {
+		List<Integer> ints = Arrays.stream(input.split("\n")).map(Integer::parseInt).collect(Collectors.toList());
+
+		int frequency = 0;
+		for (Integer integer : ints) {
+			if (integer == null) {
+				continue;
+			}
+			frequency += integer;
+		}
+		return frequency + "";
+	}
+
+	public void initializeInput() {
+		try {
+			Path path = Paths.get(getClass().getClassLoader().getResource(getInputLocation()).toURI());
+
+			Stream<String> lines = Files.lines(path);
+			input = lines.collect(Collectors.joining("\n"));
+			lines.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	protected String getInputLocation() {
+		return "inputs/day_1_1.txt";
 	}
 
 }
